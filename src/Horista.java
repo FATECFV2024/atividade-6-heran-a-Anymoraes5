49a719e48e0f1c35de9fe64dc4bd91eb994fcce7
Anymoraes5 <moraesany44@gmail.com>
2024-11-27 11:49:34 -0300

    [fix] arquivo horista

---
+++ src/Horista.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Constructor;
+
+public class Horista extends Empregado{
+    private double horas;
+
+    //construtor
+    public Horista(String n, String e, double h){
+        super(n, e);
+        horas = h;
+    }
+
+    public double getHoras() {
+        return horas;
+    }
+
+    public void setHoras(double horas) {
+        this.horas = horas;
+    }
+    
+    public void calcularSalario(){
+        salario = horas * 50;
+    }
+
+}
